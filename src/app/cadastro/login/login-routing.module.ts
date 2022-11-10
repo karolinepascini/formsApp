96f802a88cff52001d595cb83e2d2a7b3825cfe2
Karoline Pascini <61022117+karolinepascini@users.noreply.github.com>
2022-11-10 15:06:18 -0300

    10/11

--- src/app/cadastro/login/login-routing.module.ts
+++ src/app/cadastro/login/login-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginPage } from './login.page';
+import { RegistroPage } from '../registro/registro.page';
+import { Tab1Page } from '../../tab1/tab1.page';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginPage
+  },
+  {
+    path: 'registro',
+    component: RegistroPage
+  },
+  {
+    path: 'tabs/tab1',
+    component: Tab1Page
   }
 ];
 
