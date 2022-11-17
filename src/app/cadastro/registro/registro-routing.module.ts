078afdd915359134e38b78dfdc804174c68dcda3
Karoline Pascini <61022117+karolinepascini@users.noreply.github.com>
2022-11-17 14:52:41 -0300

    17/11

--- src/app/cadastro/registro/registro-routing.module.ts
+++ src/app/cadastro/registro/registro-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { RegistroPage } from './registro.page';
+import { LoginPage } from '../login/login.page';
 
 const routes: Routes = [
   {
     path: '',
     component: RegistroPage
+  },
+  {
+    path: 'login',
+    component: LoginPage
   }
 ];
 
